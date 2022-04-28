d68b4f85d898281b8671375376001ba3813f3b96
iggy <allyson.nostrandd@gmail.com>
2022-04-27 18:29:52 -0700

    setting up notes route

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get("/*", (req, res) =>{
     res.sendFile(path.join(__dirname, 'public/assets/index.html'));
 })
 
-app.get("/api/notes", (req,res) =>{
-    res.json(notes);
+app.get("/notes", (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public/assets/notes.html'));
 })
 
 app.listen(PORT, () =>{
-    console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`);
 })
