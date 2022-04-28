a36e4f98412c852fa7db9a37f55b19fe5a5ec7da
iggy <allyson.nostrandd@gmail.com>
2022-04-27 18:11:37 -0700

    port is set up along with displaying index.html on *

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path')
+const notes = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
-const allRoutes = require("./routes")
+
+app.get("/*", (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public/assets/index.html'));
+})
+
+app.get("/api/notes", (req,res) =>{
+    res.json(notes);
+})
+
+app.listen(PORT, () =>{
+    console.log(`App listening at http://localhost:${PORT}`)
+})
