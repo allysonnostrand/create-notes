f35d129056192b46ea485d3d76b2d74762042464
iggy <allyson.nostrandd@gmail.com>
2022-04-28 09:12:21 -0700

    notes route works, api/notes returns saved notes

--- server.js
+++ server.js
@@ -4,18 +4,27 @@ const path = require('path')
 const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
+/* middleware--------------------------------------*/
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('/public'));
 
-app.get("/*", (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public/assets/index.html'));
+/* homepage route---------------------------------*/
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-app.get("/notes", (req,res) =>{
-    res.sendFile(path.join(__dirname, 'public/assets/notes.html'));
+/* notetaking route--------------------------------*/
+app.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
+/* acessing notes database--------------------------*/
+app.get("/api/notes", (req,res) =>{
+    res.json(notes);
+})
+
+/* listening to port--------------------------------*/
 app.listen(PORT, () =>{
     console.log(`App listening at http://localhost:${PORT}`);
 })
